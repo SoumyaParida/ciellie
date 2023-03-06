51bf561bad7eb21decee2e20115f5789e18e7f67
aistudios-in <aistudiosdotin@gmail.com>
2023-03-07 02:28:01 +0530

    initial branch create

---
+++ lib/screens/register_page.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:login/screens/signin_page.dart';
+import '../widgets/widget.dart';
+import '../constants.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  bool passwordVisibility = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kBackgroundColor,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Image(
+            width: 24,
+            color: Colors.white,
+            image: AssetImage('assets/images/back_arrow.png'),
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: Column(
+                  children: [
+                    Flexible(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Register",
+                            style: kHeadline,
+                          ),
+                          Text(
+                            "\nCreate new account to get started.",
+                            style: kBodyText2,
+                          ),
+                          SizedBox(
+                            height: 50,
+                          ),
+                          MyTextField(
+                            hintText: 'Name',
+                            inputType: TextInputType.name,
+                          ),
+                          MyTextField(
+                            hintText: 'Email',
+                            inputType: TextInputType.emailAddress,
+                          ),
+                          MyTextField(
+                            hintText: 'Phone',
+                            inputType: TextInputType.phone,
+                          ),
+                          MyPasswordField(
+                            isPasswordVisible: passwordVisibility,
+                            onTap: () {
+                              setState(() {
+                                passwordVisibility = !passwordVisibility;
+                              });
+                            },
+                          )
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Already have an account? ",
+                          style: kBodyText,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              CupertinoPageRoute(
+                                builder: (context) => SignInPage(),
+                              ),
+                            );
+                          },
+                        child: Text(
+                          "Sign In",
+                          style: kBodyText.copyWith(
+                            color: Colors.white,
+                          ),
+                        ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    MyTextButton(
+                      buttonName: 'Register',
+                      onTap: () {},
+                      bgColor: Colors.white,
+                      textColor: Colors.black87,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
