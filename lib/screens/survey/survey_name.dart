4c4f19e40b0a34dd711ab1f66241a86d8b1cfce9
aistudios-in <aistudiosdotin@gmail.com>
2023-03-27 14:13:37 +0530

    listing survey details in survey manager

---
+++ lib/screens/survey/survey_name.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class GetSurveyName extends StatelessWidget {
+  final String documentId;
+  final String profileId;
+
+  GetSurveyName({required this.documentId, required this.profileId});
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    
+    CollectionReference surveys = FirebaseFirestore.instance.collection("surveys").doc(profileId).collection("survey");
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: surveys.doc(documentId).get(),
+      builder: ((context, snapshot){
+        if (snapshot.connectionState == ConnectionState.done) {
+        Map<String, dynamic> data = 
+                    snapshot.data!.data() as Map<String, dynamic>;
+        return Text("${data['address']}");
+      }
+      return Text("loading");
+  }),
+  );
+  }
+}
